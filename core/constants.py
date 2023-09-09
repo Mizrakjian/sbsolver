8deb000d6e14296c9595387f302ee59fe5d69a01
Mizrakjian <81772864+Mizrakjian@users.noreply.github.com>
2023-09-08 23:59:46 -0400

    Update constants.py for wonky commit

--- core/constants.py
+++ core/constants.py
@@ -1,12 +1,4 @@
 from pathlib import Path
 
-VERSION = "1.5.3"
-
-GAME_URL = "https://www.nytimes.com/puzzles/spelling-bee"
-WORDLIST_URL = "https://www.wordgamedictionary.com/twl06/download/twl06.txt"
-DATAMUSE_URL = "https://api.datamuse.com/words"
-
-DATA_PATH = Path(__file__).parent / "data"
+DATA_PATH = Path(__file__).parent.parent / "data"
 WORDS_DB = DATA_PATH / "words.db"
-
-MAX_LINE_WIDTH = 72
